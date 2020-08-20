b6dae24b019da3f6ed25c3b7087080eaf8dbd769
Sehaj Chawla <42678107+sehajchawla@users.noreply.github.com>
2020-08-20 16:11:13 +0100

    Add files via upload

---
+++ abjad_spyder.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 15:09:20 2019
+
+@author: Admin
+"""
+
+from abjad import *
+import abjad.top
+notes = []
+
+note = abjad.Note(0, Duration(1,16))
+abjad.show(note) 
+
+"""
+rest = abjad.Rest('r16')
+#abjad.show(rest)
+
+note = Note(0, Duration(16, 32))
+#note.written_duration = Duration(1, 4)
+notes.append(note)
+notes.append(rest)
+
+
+staff = abjad.Staff(notes)
+abjad.show(staff)
+"""
+
